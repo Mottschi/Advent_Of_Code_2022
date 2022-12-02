5fbdb4148f11ce84683077236f4684739841b7b5
Sebastian Mottschall <srophkor@gmail.com>
2022-12-02 22:00:46 +0100

    Day 1+2

--- day01.py
+++ day01.py
@@ -1,35 +1,43 @@
 import time
 
-DEBUG = True
+DEBUG = False
 
 if DEBUG:
 	FILE = 'day01_debug.txt'
 else:
 	FILE = 'day01.txt'
 
-
 def load():
+	data = []
 	with open(FILE) as file:
-		arr = file.read().rstrip().split('\n')
-	return arr
-
-
-def parse(data):
+		arr = file.read().rstrip().split('\n\n')
+		for line in arr:
+			line = line.split('\n')
+			line = [int(num) for num in line]
+			data.append(line)
 	return data
 
 
 def part_one(data):
-	pass
+	maximum = 0
+	for elf in data:
+		summe = sum(elf)
+		maximum = max(maximum, summe)
+	return maximum
 
 
 def part_two(data):
-	pass
+	summen = []
+	for elf in data:
+		summe = sum(elf)
+		summen.append(summe)
+	summen = sorted(summen, reverse=True)
+	summe = sum(summen[0:3])
+	return summe
 
 
 if __name__ == '__main__':
 	data = load()
-	data = parse(data)
-	print(data)
 
 	start_time = time.time()
 	print('Part One:', part_one(data))
--- day02.py
+++ day02.py
@@ -1,6 +1,6 @@
 import time
 
-DEBUG = True
+DEBUG = False
 
 if DEBUG:
 	FILE = 'day02_debug.txt'
@@ -15,15 +15,58 @@ def load():
 
 
 def parse(data):
-	return data
+	return [line.split() for line in data]
 
+player_one = {'A': 'rock', 'B': 'paper', 'C': 'scissors'}
+player_two = {'X': 'rock', 'Y': 'paper', 'Z': 'scissors'}
 
-def part_one(data):
-	pass
+game_score = {('rock', 'paper'): 6,
+		 ('rock', 'scissors'): 0,
+		 ('rock', 'rock'): 3,
+		 ('paper', 'paper'): 3,
+		 ('paper', 'scissors'): 6,
+		 ('paper', 'rock'): 0,
+		 ('scissors', 'paper'): 0,
+		 ('scissors', 'scissors'): 3,
+		 ('scissors', 'rock'): 6,
+		 }
 
+figure_score = {'rock': 1, 'paper': 2, 'scissors': 3}
 
-def part_two(data):
-	pass
+game_score_part2 = {
+	'X': 0,
+	'Y': 3,
+	'Z': 6,
+}
+
+def part_one(games):
+
+	score = 0
+	for elfs, mine in games:
+		elf = player_one[elfs]
+		me = player_two[mine]
+		score += figure_score[me] + game_score[(elf, me)]
+	return score
+
+def part_two(games):
+	player_two = {
+
+		('rock', 'X'): 'scissors',
+		('rock', 'Y'): 'rock',
+		('rock', 'Z'): 'paper',
+		('paper', 'X'): 'rock',
+		('paper', 'Y'): 'paper',
+		('paper', 'Z'): 'scissors',
+		('scissors', 'X'): 'paper',
+		('scissors', 'Y'): 'scissors',
+		('scissors', 'Z'): 'rock',
+	}
+	score = 0
+	for elfs, mine in games:
+		elf = player_one[elfs]
+		me = player_two[(elf, mine)]
+		score += figure_score[me] + game_score_part2[mine]
+	return score
 
 
 if __name__ == '__main__':
