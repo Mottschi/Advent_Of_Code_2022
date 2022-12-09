d41dde51cad6a1557795c50e417a014bce18f21c
Sebastian Mottschall <srophkor@gmail.com>
2022-12-09 10:27:24 +0100

    day 8 and 9

--- day08.py
+++ day08.py
@@ -1,6 +1,6 @@
 import time
 
-DEBUG = True
+DEBUG = 0
 
 if DEBUG:
 	FILE = 'day08_debug.txt'
@@ -15,16 +15,124 @@ def load():
 
 
 def parse(data):
-	return data
+	result = []
+	for line in data:
+		result.append(list(line))
+	return result
 
 
-def part_one(data):
-	pass
+def part_one(grid):
+	visible_trees = set()
+	invisible_trees = set()
+	for row in range (0, len(grid)):
+		for col in range(0, len(grid[row])):
+			tree_pos = row, col
+			# all trees on the border are visible
+			if row == 0 or col == 0 or row == len(grid) - 1 or col == len(grid[row]) - 1:
+				visible_trees.add(tree_pos)
+				continue
+
+			# for all other trees, we need to check whether they are visible from any of the four directions
+			# is there any tree that is bigger to the left?
+			# the moment we find that a tree that is bigger, we know the current tree is hidden, until then we assume
+			# it is visible
+			current_tree_height = grid[row][col]
+			visible = True
+			# print(f'\nchecking on tree {tree_pos} of size {current_tree_height}')
+			for i in range(0, col):
+				# print('checking ')
+				if grid[row][i] >= current_tree_height:
+					visible = False
+					# print(f'found a tree bigger then size {current_tree_height} to the left, at {row},{i}, size: {grid[row][i]}')
+					break
+
+			# if, after checking all trees to one side, the tree is visible (we havent found any bigger tree), we add it
+			# to the list of visible trees and skip checking the other directions
+			if visible:
+				visible_trees.add(tree_pos)
+				continue
+
+			# is there any tree that is bigger to the right?
+			visible = True
+			for i in range(col+1, len(grid[row])):
+				if grid[row][i] >= current_tree_height:
+					visible = False
+					# print(f'found a tree bigger then size {current_tree_height} to the right, at {row},{i}, size: {grid[row][i]}')
+					break
+			if visible:
+				visible_trees.add(tree_pos)
+				continue
+			# is there any tree that is bigger to the top?
+			visible = True
+			for i in range(0, row):
+				if grid[i][col] >= current_tree_height:
+					visible = False
+					# print(f'found a tree bigger then size {current_tree_height} to the top, at {i},{col}, size: {grid[i][col]}')
+					break
+			if visible:
+				visible_trees.add(tree_pos)
+				continue
+
+			# is there any tree that is bigger to the bottom?
+			visible = True
+			for i in range(row+1, len(grid)):
+				if grid[i][col] >= current_tree_height:
+					visible = False
+					# print(f'found a tree bigger then size {current_tree_height} to the bottom, at {i},{col}, size: {grid[i][col]}')
+					break
+			if visible:
+				visible_trees.add(tree_pos)
+				# print('tree is visible, adding to list')
+				continue
+			# print('tree is invisible')
+			invisible_trees.add(tree_pos)
+	return len(visible_trees)
 
 
-def part_two(data):
-	pass
 
+
+
+def part_two(grid):
+	height = len(grid)
+	width = len(grid[0])
+	max_score = 0
+	for row in range(1, height-1):
+		for col in range(1, width-1):
+			tree = {'row': row, 'col': col, 'height': grid[row][col]}
+
+			score_left = 0
+			score_right = 0
+			score_up = 0
+			score_down = 0
+
+			for i in range(col-1, -1, -1):
+				c_row, c_col = row, i
+				score_left += 1
+				if grid[c_row][c_col] >= tree['height']:
+					break
+
+			for i in range(col+1, width):
+				c_row, c_col = row, i
+				score_right += 1
+				if grid[c_row][c_col] >= tree['height']:
+					break
+
+			for i in range(row-1, -1, -1):
+				c_row, c_col = i, col
+				score_up += 1
+				if grid[c_row][c_col] >= tree['height']:
+					break
+
+			for i in range(row+1, height):
+				c_row, c_col = i, col
+				score_down += 1
+				if grid[c_row][c_col] >= tree['height']:
+					break
+
+			tree_score = score_up * score_down * score_right * score_left
+			if tree_score > max_score:
+				max_score = tree_score
+	return max_score
 
 if __name__ == '__main__':
 	data = load()
--- day09.py
+++ day09.py
@@ -1,9 +1,9 @@
 import time
 
-DEBUG = True
+DEBUG = 0
 
 if DEBUG:
-	FILE = 'day09_debug.txt'
+	FILE = 'day09_debug2.txt'
 else:
 	FILE = 'day09.txt'
 
@@ -15,15 +15,121 @@ def load():
 
 
 def parse(data):
-	return data
+	movement = [(direction, int(step)) for direction, step in  [line.split() for line in data]]
+	return movement
+
+def distance(head, tail):
+	x1, y1 = head
+	x2, y2 = tail
+	return abs(x1-x2) + abs(y1-y2)
+
+def touching(head, tail):
+	d = distance(head, tail)
+	if d < 2:
+		return True
+	if d > 2:
+		return False
+	return head[0] != tail[0] and head[1] != tail[1]
+
+def part_one(h_movement):
+	tail = [0, 0]
+	head = [0, 0]
+
+	visited = set()
+	visited.add((tail[0], tail[1]))
+
+	# Head movement
+	for direction, steps in h_movement:
+		for _ in range(steps):
+			match direction:
+				case 'R':
+					head[0] += 1
+				case 'L':
+					head[0] -= 1
+				case 'U':
+					head[1] += 1
+				case 'D':
+					head[1] -= 1
+
+			# Tail movement
+			if touching(head, tail):
+				# still touching, tail not moving
+				continue
+			if tail[0] == head[0]:
+				if head[1] > tail[1]:
+					tail[1] += 1
+				else:
+					tail[1] -= 1
+			elif tail[1] == head[1]:
+				if head[0] > tail[0]:
+					tail[0] += 1
+				else:
+					tail[0] -= 1
+			else:
+				if head[0] > tail[0]:
+					tail[0] += 1
+				else:
+					tail[0] -= 1
+				if head[1] > tail[1]:
+					tail[1] += 1
+				else:
+					tail[1] -= 1
+
+			visited.add((tail[0], tail[1]))
+	return len(visited)
 
 
-def part_one(data):
-	pass
 
+def part_two(h_movement):
+	print('part 2 start')
+	tails = [[0, 0] for _ in range(10)]
 
-def part_two(data):
-	pass
+	visited = set()
+	visited.add((0, 0))
+
+	# Head movement
+	head = tails[0]
+	for direction, steps in h_movement:
+		for _ in range(steps):
+			head = tails[0]
+			match direction:
+				case 'R':
+					head[0] += 1
+				case 'L':
+					head[0] -= 1
+				case 'U':
+					head[1] += 1
+				case 'D':
+					head[1] -= 1
+
+			for i in range(1, len(tails)):
+				head = tails[i-1]
+				tail = tails[i]
+				# Tail movement
+				if touching(head, tail):
+					# still touching, tail not moving
+					continue
+				if tail[0] == head[0]:
+					if head[1] > tail[1]:
+						tail[1] += 1
+					else:
+						tail[1] -= 1
+				elif tail[1] == head[1]:
+					if head[0] > tail[0]:
+						tail[0] += 1
+					else:
+						tail[0] -= 1
+				else:
+					if head[0] > tail[0]:
+						tail[0] += 1
+					else:
+						tail[0] -= 1
+					if head[1] > tail[1]:
+						tail[1] += 1
+					else:
+						tail[1] -= 1
+			visited.add((tails[-1][0], tails[-1][1]))
+	return len(visited)
 
 
 if __name__ == '__main__':
