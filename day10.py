40f58cfdf92bd4bb675e21550fcfbdff84d2d345
Sebastian Mottschall <srophkor@gmail.com>
2022-12-10 09:46:07 +0100

    Day 10

--- day10.py
+++ day10.py
@@ -1,9 +1,9 @@
 import time
 
-DEBUG = True
+DEBUG = 0
 
 if DEBUG:
-	FILE = 'day10_debug.txt'
+	FILE = 'day10_debug2.txt'
 else:
 	FILE = 'day10.txt'
 
@@ -15,15 +15,47 @@ def load():
 
 
 def parse(data):
-	return data
+	return [(element[0],) if len(element) == 1 else (element[0], int(element[1])) for element in [line.split() for line in data]]
 
 
-def part_one(data):
-	pass
+def part_one(instructions):
+	x = 1
+	cycle = 0
+	signal_sum = 0
+	for line in instructions:
+		instruction = line[0]
+		cycles = len(line)
+		for c in range(cycles):
+			cycle += 1
+			if cycle % 40 == 20:
+				signal_strength = cycle * x
+				signal_sum += signal_strength
+
+		if instruction == 'addx':
+			x += line[1]
+	print('cycles total: ', cycle)
+	return signal_sum
 
 
-def part_two(data):
-	pass
+def part_two(instructions):
+	x = 1
+	cycle = 0
+	signal_sum = 0
+	for line in instructions:
+		instruction = line[0]
+		cycles = len(line)
+		for c in range(cycles):
+			if abs(x - cycle % 40) > 1:
+				char = '.'
+			else:
+				char = '#'
+			print(char, end='')
+			cycle += 1
+			if cycle % 40 == 0:
+				print()
+		if instruction == 'addx':
+			x += line[1]
+	return signal_sum
 
 
 if __name__ == '__main__':
