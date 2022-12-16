19b11413a5b3068488d09b64b251c985eb6e3293
Sebastian Mottschall <srophkor@gmail.com>
2022-12-16 23:07:59 +0100

    day 16 WIP

--- day16.py
+++ day16.py
@@ -1,4 +1,5 @@
 import time
+import re
 
 DEBUG = True
 
@@ -15,11 +16,28 @@ def load():
 
 
 def parse(data):
-	return data
+	valve_controls = []
+	valves = []
+	result = {}
+	result['valves'] = valves
+	result['controls'] = valve_controls
+	for line in data:
+		line = re.search('^Valve ([A-Z]{2}) has flow rate=(\d+); tunnel(?:s)? lead(?:s)? to valve(?:s)? ((?:[A-Z]{2}(?:,\s)?)+)$', line)
+		valve = line.group(1)
+		flow_rate = int(line.group(2))
+		tunnels = line.group(3).split(', ')
+		valve_controls.append((valve, flow_rate, tunnels))
+		valves.append(valve)	
+	return result
 
 
 def part_one(data):
-	pass
+	current_room = 'aa'
+	current_flow = 0
+	status = {valve: False for valve in data['valves']}
+	queue = [(current_room, current_flow, status)]
+	valves = data['valves']
+		
 
 
 def part_two(data):
